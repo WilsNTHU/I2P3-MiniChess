301405afcc2d8bb9d6055057d9d0eb747deea855
WilsNTHU <wilsonlin001@gmail.com>
2023-06-19 17:26:10 +0800

    Version 2 Minimax
    Ver 2 of Minimax, improved algorithm,
    for tested use.

--- src/policy/minimax.cpp
+++ src/policy/minimax.cpp
@@ -14,25 +14,26 @@
  */
 
 
-int Minimax::miniMax(State *state, int depth, int isMaxPlayer){
+int Minimax::miniMax(State *state, int depth, int isMinPlayer){
     if (depth == 0) {
         return -state->evaluate();
     }
 
     auto actions = state->legal_actions;
 
-    if (isMaxPlayer) {
-        int bestMove = -9999;
-        for (auto it = actions.begin(); it != actions.end(); it++) {
-            State *new_state = state->next_state(*it);
-            bestMove = std::max(bestMove, miniMax(new_state, depth - 1, 1 - isMaxPlayer));
-        }
-        return bestMove;
-    } else {
+    if (isMinPlayer) {
         int bestMove = 9999;
         for(auto it = actions.begin(); it != actions.end(); it++) {
             State *new_state = state->next_state(*it);
-            bestMove = std::min(bestMove, miniMax(new_state, depth - 1, 1 - isMaxPlayer));
+            bestMove = std::min(bestMove, miniMax(new_state, depth - 1, 1 - isMinPlayer));
+        }
+        return bestMove;
+        
+    } else {
+        int bestMove = -9999;
+        for (auto it = actions.begin(); it != actions.end(); it++) {
+            State *new_state = state->next_state(*it);
+            bestMove = std::max(bestMove, miniMax(new_state, depth - 1, 1 - isMinPlayer));
         }
         return bestMove;
     }
@@ -48,7 +49,7 @@ Move Minimax::get_move(State *state, int depth){
 
     for(auto it = actions.begin(); it != actions.end(); it++) {
         State *new_state = state->next_state(*it);
-        int value = miniMax(new_state, depth - 1, 1-state->player);
+        int value = miniMax(new_state, depth - 1, 1 - state->player);
         if(value >= bestMove) {
             bestMove = value;
             bestMoveFound = *it;
